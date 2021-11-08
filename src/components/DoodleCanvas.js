ee36dc0cf45066df2eac24b3acfd8534ddf605ff
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-11-08 08:39:17 -0800

    Get basic draw

--- src/components/DoodleCanvas.js
+++ src/components/DoodleCanvas.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-
 export default function DoodleCanvas(props) {
   const canvasRef = React.useRef(null);
   let ctx = null;
@@ -9,20 +8,21 @@ export default function DoodleCanvas(props) {
   const [drawing, setDrawing] = React.useState(false);
   const [coords, setCoords] = React.useState({ x: null, y: null });
   // const [previousOffsets, setPreviousOffsets] = React.useState([]);
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvasRef.current.getContext("2d");
+  }, []);
 
-  function draw(e) {
+  function draw(ctx, e) {
     console.log(e);
     console.log("drawing");
     ctx.strokeStyle = `hsla(${props.hue}, ${props.saturation}%, ${props.lightness}%, ${props.opacity}%)`;
     ctx.moveTo(coords.x, coords.y);
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
+    setCoords({ x: e.offsetX, y: e.offsetY });
 
   }
-
-  React.useEffect(() => {
-    ctx = canvasRef.current.getContext("2d");
-  }, []);
 
   return (
     <canvas
@@ -37,7 +37,8 @@ export default function DoodleCanvas(props) {
       }}
       onMouseMove={(e) => {
         if (drawing) {
-          draw(e.nativeEvent);
+          const ctx = canvasRef.current.getContext("2d");
+          draw(ctx,e.nativeEvent);
         }
       }}
       onMouseUp={(e) => {
@@ -52,4 +53,4 @@ export default function DoodleCanvas(props) {
 
 DoodleCanvas.propTypes = {
   tools: PropTypes.object,
-}
+};
