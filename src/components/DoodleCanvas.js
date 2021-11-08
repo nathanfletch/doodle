91cddf966faa5eea8094430b62c86967c016d7b7
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-11-07 22:38:04 -0800

    Add canvas hooks and handlers

--- src/components/DoodleCanvas.js
+++ src/components/DoodleCanvas.js
@@ -2,16 +2,36 @@ import React from "react";
 
 export default function DoodleCanvas() {
   const canvasRef = React.useRef(null)
+
+  const [drawing, setDrawing] = React.useState(false);
+  // const [coords, setCoords] = React.useState({x: null, y: null});
+  // const [previousOffsets, setPreviousOffsets] = React.useState([]);
+  
+  React.useEffect(() => {
+    canvasRef.current.getContext('2d')
+  }, [])
+
+
   return (
     <canvas
       width={window.innerWidth}
       height={window.innerHeight}
-      onClick={(e) => {
-        console.log(e);
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
-        console.log(ctx);
+      
+      onMouseDown ={(e) => {
+        console.log("down");
+        console.log(`drawing: ${drawing}`);
+        setDrawing(true);
       }}
+      onMouseMove ={(e) => {
+        if(drawing) 
+        console.log("drawing");
+      }}
+      onMouseUp ={(e) => {
+        console.log("up");
+        console.log(`drawing: ${drawing}`);
+        setDrawing(false);
+      }}
+      ref={canvasRef}
       id="doodle-canvas"
     ></canvas>
   );
