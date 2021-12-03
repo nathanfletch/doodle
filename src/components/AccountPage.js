9d9d01e8c1f2790331471ede948909088be0bcd8
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-12-03 14:54:05 -0800

    Rearrange AccountPage and children

--- src/components/AccountPage.js
+++ src/components/AccountPage.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+// import { Link as MuiLink } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+
 // import PropTypes from 'prop-types'
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="https://github.com/nathanfletch"
+        target="blank"
+      >
+        Nathan Fletcher
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 function AccountPage(props) {
+  const [mode, setMode] = useState("signup");
   return (
     <>
-      <SignUp />
+      {mode === "signup" ? (
+        <SignUp setMode={setMode} />
+      ) : (
+        <SignIn setMode={setMode} />
+      )}
+      <Copyright sx={{ mt: 5 }} />
     </>
   );
 }
